56172b43c7fa71229636a0c739d4eb925bbd2cc8
stefangabos <contact@stefangabos.ro>
2013-12-09 20:59:30 +0200

    Integration with Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,50 @@
+module.exports = function(grunt) {
+
+    var source_files = ['public/javascript/zebra_datepicker.src.js'];
+
+    grunt.initConfig({
+
+        pkg: grunt.file.readJSON('package.json'),
+
+        // configure plugins
+
+        'uglify': {
+            options: {
+            },
+            build: {
+                src: source_files,
+                dest: 'public/javascript/zebra_datepicker.js'
+            }
+        },
+
+        'jshint': {
+            files: source_files,
+            options: {
+                browser: true,
+                indent: false,
+                quotmark: 'single',
+                strict: true,
+                trailing: true,
+                loopfunc: true,
+                scripturl: true
+            }
+        },
+
+        'watch': {
+            files: source_files,
+            tasks: ['jshint', 'uglify']
+        }
+
+    });
+
+    // register plugins
+
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+
+    // register tasks
+
+    grunt.registerTask('default', ['uglify']);
+
+};
