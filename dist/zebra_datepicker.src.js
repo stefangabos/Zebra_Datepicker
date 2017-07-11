stefangabos/Zebra_Datepicker

commit 2bf9adf84a2014a245f432744c660aa160004380

Revert  changes done by 4ac8bbc in order to support older IE browsers. Fixes #88

dist/zebra_datepicker.src.js
@@ -506,7 +506,7 @@
             if (views.length === 0) views = ['years', 'months', 'days'];
 
             // if the starting view is not amongst the views the user can cycle through, set the correct starting view
-            if (views.indexOf(plugin.settings.view) === -1) plugin.settings.view = views[views.length - 1];
+            if ($.inArray(plugin.settings.view, views) === -1) plugin.settings.view = views[views.length - 1];
 
             // parse the rules for disabling dates and turn them into arrays of arrays
 
@@ -542,13 +542,13 @@
 
                             // if rule contains a comma, create a new array by splitting the rule by commas
                             // if there are no commas create an array containing the rule's string
-                            rules[i] = (rules[i].indexOf(',') > -1 ? rules[i].split(',') : new Array(rules[i]));
+                            rules[i] = ($.inArray(',', rules[i]) > -1 ? rules[i].split(',') : new Array(rules[i]));
 
                             // iterate through the items in the rule
                             for (j = 0; j < rules[i].length; j++)
 
                                 // if item contains a dash (defining a range)
-                                if (rules[i][j].indexOf('-') > -1) {
+                                if ($.inArray('-', rules[i][j]) > -1) {
 
                                     // get the lower and upper limits of the range
                                     limits = rules[i][j].match(/^([0-9]+)\-([0-9]+)/);
@@ -561,7 +561,7 @@
 
                                             // if value is not already among the values of the rule
                                             // add it to the rule
-                                            if (rules[i].indexOf(k) === -1) rules[i].push(k + '');
+                                            if ($.inArray(k, rules[i]) === -1) rules[i].push(k + '');
 
                                         // remove the range indicator
                                         rules[i].splice(j, 1);
@@ -1097,7 +1097,7 @@
 
             // if the "Today" button is to be shown and it makes sense to be shown
             // (the "days" view is available and "today" is not a disabled date)
-            show_select_today = (plugin.settings.show_select_today !== false && views.indexOf('days') > -1 && !is_disabled(current_system_year, current_system_month, current_system_day) ? plugin.settings.show_select_today : false);
+            show_select_today = (plugin.settings.show_select_today !== false && $.inArray('days', views) > -1 && !is_disabled(current_system_year, current_system_month, current_system_day) ? plugin.settings.show_select_today : false);
 
             // if we just needed to recompute the things above
             if (update) {
@@ -1235,13 +1235,13 @@
             $('.dp_caption', header).on('click', function() {
 
                 // if current view is "days", take the user to the next view, depending on the format
-                if (view === 'days') view = (views.indexOf('months') > -1 ? 'months' : (views.indexOf('years') > -1 ? 'years' : 'days'));
+                if (view === 'days') view = ($.inArray('months', views) > -1 ? 'months' : ($.inArray('years', views) > -1 ? 'years' : 'days'));
 
                 // if current view is "months", take the user to the next view, depending on the format
-                else if (view === 'months') view = (views.indexOf('years') > -1 ? 'years' : (views.indexOf('days') > -1 ? 'days' : 'months'));
+                else if (view === 'months') view = ($.inArray('years', views) > -1 ? 'years' : ($.inArray('days', views) > -1 ? 'days' : 'months'));
 
                 // if current view is "years", take the user to the next view, depending on the format
-                else view = (views.indexOf('days') > -1 ? 'days' : (views.indexOf('months') > -1 ? 'months' : 'years'));
+                else view = ($.inArray('days', views) > -1 ? 'days' : ($.inArray('months', views) > -1 ? 'months' : 'years'));
 
                 // generate the appropriate view
                 manage_views();
@@ -1301,7 +1301,7 @@
                 selected_month = to_int(matches[1]);
 
                 // if user can select only years and months
-                if ('days', views.indexOf('days') === -1)
+                if ('days', $.inArray('days', views) === -1)
 
                     // put selected date in the element the plugin is attached to, and hide the date picker
                     select_date(selected_year, selected_month, 1, 'months', $(this));
@@ -1329,7 +1329,7 @@
                 selected_year = to_int($(this).html());
 
                 // if user can select only years
-                if (views.indexOf('months') === -1)
+                if ($.inArray('months', views) === -1)
 
                     // put selected date in the element the plugin is attached to, and hide the date picker
                     select_date(selected_year, 1, 1, 'years', $(this));
@@ -1741,7 +1741,7 @@
                 for (var i = 0; i < format_chars.length; i++)
 
                     // if character is found in the date's format
-                    if ((position = format.indexOf(format_chars[i])) > -1)
+                    if ((position = $.inArray(format_chars[i], format)) > -1)
 
                         // save it, alongside the character's position
                         matches.push({
@@ -2179,7 +2179,7 @@
                     if (is_disabled(selected_year, selected_month, day)) {
 
                         // if day is in weekend
-                        if (plugin.settings.weekend_days.indexOf(weekday) > -1) class_name = 'dp_weekend_disabled';
+                        if ($.inArray(weekday, plugin.settings.weekend_days) > -1) class_name = 'dp_weekend_disabled';
 
                         // if work day
                         else class_name += ' dp_disabled';
@@ -2194,7 +2194,7 @@
                     } else {
 
                         // if day is in weekend
-                        if (plugin.settings.weekend_days.indexOf(weekday) > -1) class_name = 'dp_weekend';
+                        if ($.inArray(weekday, plugin.settings.weekend_days) > -1) class_name = 'dp_weekend';
 
                         // highlight the currently selected date
                         if (selected_month === default_month && selected_year === default_year && default_day === day) class_name += ' dp_selected';
@@ -2379,24 +2379,24 @@
                         var rule = this, weekday;
 
                         // if the rules apply for the current year
-                        if (rule[2].indexOf(year) > -1 || rule[2].indexOf('*') > -1)
+                        if ($.inArray(year, rule[2]) > -1 || $.inArray('*', rule[2]) > -1)
 
                             // if the rules apply for the current month
-                            if ((typeof month !== 'undefined' && rule[1].indexOf(month) > -1) || rule[1].indexOf('*') > -1)
+                            if ((typeof month !== 'undefined' && $.inArray(month, rule[1]) > -1) || $.inArray('*', rule[1]) > -1)
 
                                 // if the rules apply for the current day
-                                if ((typeof day !== 'undefined' && rule[0].indexOf(day) > -1) || rule[0].indexOf('*') > -1) {
+                                if ((typeof day !== 'undefined' && $.inArray(day, rule[0]) > -1) || $.inArray('*', rule[0]) > -1) {
 
                                     // if custom class is to be applied whatever the day
                                     // don't look any further
-                                    if (rule[3].indexOf('*') > -1) return (found = class_name);
+                                    if ($.inArray('*', rule[3]) > -1) return (found = class_name);
 
                                     // get the weekday
                                     weekday = new Date(year, month - 1, day).getDay();
 
                                     // if custom class is to be applied to weekday
                                     // don't look any further
-                                    if (rule[3].indexOf(weekday) > -1) return (found = class_name);
+                                    if ($.inArray(weekday, rule[3]) > -1) return (found = class_name);
 
                                 }
 
@@ -2573,24 +2573,24 @@
                     var rule = this, weekday;
 
                     // if the rules apply for the current year
-                    if (rule[2].indexOf(year) > -1 || rule[2].indexOf('*') > -1)
+                    if ($.inArray(year, rule[2]) > -1 || $.inArray('*', rule[2]) > -1)
 
                         // if the rules apply for the current month
-                        if ((typeof month !== 'undefined' && rule[1].indexOf(month) > -1) || rule[1].indexOf('*') > -1)
+                        if ((typeof month !== 'undefined' && $.inArray(month, rule[1]) > -1) || $.inArray('*', rule[1]) > -1)
 
                             // if the rules apply for the current day
-                            if ((typeof day !== 'undefined' && rule[0].indexOf(day) > -1) || rule[0].indexOf('*') > -1) {
+                            if ((typeof day !== 'undefined' && $.inArray(day, rule[0]) > -1) || $.inArray('*', rule[0]) > -1) {
 
                                 // if day is to be disabled whatever the day
                                 // don't look any further
-                                if (rule[3].indexOf('*') > -1) return (disabled = true);
+                                if ($.inArray('*', rule[3]) > -1) return (disabled = true);
 
                                 // get the weekday
                                 weekday = new Date(year, month - 1, day).getDay();
 
                                 // if weekday is to be disabled
                                 // don't look any further
-                                if (rule[3].indexOf(weekday) > -1) return (disabled = true);
+                                if ($.inArray(weekday, rule[3]) > -1) return (disabled = true);
 
                             }
 
@@ -2608,7 +2608,7 @@
                     var rule = this, weekday;
 
                     // if the rules apply for the current year
-                    if (rule[2].indexOf(year) > -1 || rule[2].indexOf('*') > -1) {
+                    if ($.inArray(year, rule[2]) > -1 || $.inArray('*', rule[2]) > -1) {
 
                         // the year is enabled
                         enabled = true;
@@ -2620,7 +2620,7 @@
                             enabled = true;
 
                             // if the rules apply for the current month
-                            if (rule[1].indexOf(month) > -1 || rule[1].indexOf('*') > -1) {
+                            if ($.inArray(month, rule[1]) > -1 || $.inArray('*', rule[1]) > -1) {
 
                                 // if we're also checking days
                                 if (typeof day !== 'undefined') {
@@ -2629,18 +2629,18 @@
                                     enabled = true;
 
                                     // if the rules apply for the current day
-                                    if (rule[0].indexOf(day) > -1 || rule[0].indexOf('*') > -1) {
+                                    if ($.inArray(day, rule[0]) > -1 || $.inArray('*', rule[0]) > -1) {
 
                                         // if day is to be enabled whatever the day
                                         // don't look any further
-                                        if (rule[3].indexOf('*') > -1) return (enabled = true);
+                                        if ($.inArray('*', rule[3]) > -1) return (enabled = true);
 
                                         // get the weekday
                                         weekday = new Date(year, month - 1, day).getDay();
 
                                         // if weekday is to be enabled
                                         // don't look any further
-                                        if (rule[3].indexOf(weekday) > -1) return (enabled = true);
+                                        if ($.inArray(weekday, rule[3]) > -1) return (enabled = true);
 
                                         // if we get this far, it means the day is not enabled
                                         enabled = false;
@@ -3198,14 +3198,14 @@
                     dataProp = data[i].prop;
                     this.versionSearchString = data[i].versionSearch || data[i].identity;
                     if (dataString) {
-                        if (dataString.indexOf(data[i].subString) !== -1)
+                        if ($.inArray(data[i].subString, dataString) !== -1)
                             return data[i].identity;
                     } else if (dataProp)
                         return data[i].identity;
                 }
             },
             searchVersion: function(dataString) {
-                var index = dataString.indexOf(this.versionSearchString);
+                var index = $.inArray(this.versionSearchString, dataString);
 
                 if (index === -1) return;
 

commit 665b2e550aff9171cd0c54e704faf86611853837

Fixed issues introduced in  2bf9adf

dist/zebra_datepicker.src.js
@@ -542,13 +542,13 @@
 
                             // if rule contains a comma, create a new array by splitting the rule by commas
                             // if there are no commas create an array containing the rule's string
-                            rules[i] = ($.inArray(',', rules[i]) > -1 ? rules[i].split(',') : new Array(rules[i]));
+                            rules[i] = (rules[i].indexOf(',') > -1 ? rules[i].split(',') : new Array(rules[i]));
 
                             // iterate through the items in the rule
                             for (j = 0; j < rules[i].length; j++)
 
                                 // if item contains a dash (defining a range)
-                                if ($.inArray('-', rules[i][j]) > -1) {
+                                if (rules[i][j].indexOf('-') > -1) {
 
                                     // get the lower and upper limits of the range
                                     limits = rules[i][j].match(/^([0-9]+)\-([0-9]+)/);
@@ -1301,7 +1301,7 @@
                 selected_month = to_int(matches[1]);
 
                 // if user can select only years and months
-                if ('days', $.inArray('days', views) === -1)
+                if ($.inArray('days', views) === -1)
 
                     // put selected date in the element the plugin is attached to, and hide the date picker
                     select_date(selected_year, selected_month, 1, 'months', $(this));
@@ -1741,7 +1741,7 @@
                 for (var i = 0; i < format_chars.length; i++)
 
                     // if character is found in the date's format
-                    if ((position = $.inArray(format_chars[i], format)) > -1)
+                    if ((position = rules[i][j].indexOf(format_chars[i])) > -1)
 
                         // save it, alongside the character's position
                         matches.push({
@@ -2389,7 +2389,7 @@
 
                                     // if custom class is to be applied whatever the day
                                     // don't look any further
-                                    if ($.inArray('*', rule[3]) > -1) return (found = class_name);
+                                    if (rule[3].indexOf('*') > -1) return (found = class_name);
 
                                     // get the weekday
                                     weekday = new Date(year, month - 1, day).getDay();
@@ -2583,7 +2583,7 @@
 
                                 // if day is to be disabled whatever the day
                                 // don't look any further
-                                if ($.inArray('*', rule[3]) > -1) return (disabled = true);
+                                if (rule[3].indexOf('*') > -1) return (disabled = true);
 
                                 // get the weekday
                                 weekday = new Date(year, month - 1, day).getDay();
@@ -2633,7 +2633,7 @@
 
                                         // if day is to be enabled whatever the day
                                         // don't look any further
-                                        if ($.inArray('*', rule[3]) > -1) return (enabled = true);
+                                        if (rule[3].indexOf('*') > -1) return (enabled = true);
 
                                         // get the weekday
                                         weekday = new Date(year, month - 1, day).getDay();
@@ -3198,14 +3198,14 @@
                     dataProp = data[i].prop;
                     this.versionSearchString = data[i].versionSearch || data[i].identity;
                     if (dataString) {
-                        if ($.inArray(data[i].subString, dataString) !== -1)
+                        if (dataString.indexOf(data[i].subString) !== -1)
                             return data[i].identity;
                     } else if (dataProp)
                         return data[i].identity;
                 }
             },
             searchVersion: function(dataString) {
-                var index = $.inArray(this.versionSearchString, dataString);
+                var index = dataString.indexOf(this.versionSearchString);
 
                 if (index === -1) return;
 

src/zebra_datepicker.src.js
@@ -542,13 +542,13 @@
 
                             // if rule contains a comma, create a new array by splitting the rule by commas
                             // if there are no commas create an array containing the rule's string
-                            rules[i] = ($.inArray(',', rules[i]) > -1 ? rules[i].split(',') : new Array(rules[i]));
+                            rules[i] = (rules[i].indexOf(',') > -1 ? rules[i].split(',') : new Array(rules[i]));
 
                             // iterate through the items in the rule
                             for (j = 0; j < rules[i].length; j++)
 
                                 // if item contains a dash (defining a range)
-                                if ($.inArray('-', rules[i][j]) > -1) {
+                                if (rules[i][j].indexOf('-') > -1) {
 
                                     // get the lower and upper limits of the range
                                     limits = rules[i][j].match(/^([0-9]+)\-([0-9]+)/);
@@ -1301,7 +1301,7 @@
                 selected_month = to_int(matches[1]);
 
                 // if user can select only years and months
-                if ('days', $.inArray('days', views) === -1)
+                if ($.inArray('days', views) === -1)
 
                     // put selected date in the element the plugin is attached to, and hide the date picker
                     select_date(selected_year, selected_month, 1, 'months', $(this));
@@ -1741,7 +1741,7 @@
                 for (var i = 0; i < format_chars.length; i++)
 
                     // if character is found in the date's format
-                    if ((position = $.inArray(format_chars[i], format)) > -1)
+                    if ((position = rules[i][j].indexOf(format_chars[i])) > -1)
 
                         // save it, alongside the character's position
                         matches.push({
@@ -2389,7 +2389,7 @@
 
                                     // if custom class is to be applied whatever the day
                                     // don't look any further
-                                    if ($.inArray('*', rule[3]) > -1) return (found = class_name);
+                                    if (rule[3].indexOf('*') > -1) return (found = class_name);
 
                                     // get the weekday
                                     weekday = new Date(year, month - 1, day).getDay();
@@ -2583,7 +2583,7 @@
 
                                 // if day is to be disabled whatever the day
                                 // don't look any further
-                                if ($.inArray('*', rule[3]) > -1) return (disabled = true);
+                                if (rule[3].indexOf('*') > -1) return (disabled = true);
 
                                 // get the weekday
                                 weekday = new Date(year, month - 1, day).getDay();
@@ -2633,7 +2633,7 @@
 
                                         // if day is to be enabled whatever the day
                                         // don't look any further
-                                        if ($.inArray('*', rule[3]) > -1) return (enabled = true);
+                                        if (rule[3].indexOf('*') > -1) return (enabled = true);
 
                                         // get the weekday
                                         weekday = new Date(year, month - 1, day).getDay();
@@ -3198,14 +3198,14 @@
                     dataProp = data[i].prop;
                     this.versionSearchString = data[i].versionSearch || data[i].identity;
                     if (dataString) {
-                        if ($.inArray(data[i].subString, dataString) !== -1)
+                        if (dataString.indexOf(data[i].subString) !== -1)
                             return data[i].identity;
                     } else if (dataProp)
                         return data[i].identity;
                 }
             },
             searchVersion: function(dataString) {
-                var index = $.inArray(this.versionSearchString, dataString);
+                var index = dataString.indexOf(this.versionSearchString);
 
                 if (index === -1) return;
 