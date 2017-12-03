dcd53b3e8c2ac622ee19470bbb621225a31ead67
Stefan Gabos <contact@stefangabos.ro>
2017-12-03 21:59:55 +0200

    Updated version number

--- dist/zebra_datepicker.src.js
+++ dist/zebra_datepicker.src.js
@@ -6,7 +6,7 @@
  *  Read more {@link https://github.com/stefangabos/Zebra_Datepicker/ here}
  *
  *  @author     Stefan Gabos <contact@stefangabos.ro>
- *  @version    1.9.6 (last revision: November 27, 2017)
+ *  @version    1.9.7 (last revision: December 03, 2017)
  *  @copyright  (c) 2011 - 2017 Stefan Gabos
  *  @license    http://www.gnu.org/licenses/lgpl-3.0.txt GNU LESSER GENERAL PUBLIC LICENSE
  *  @package    Zebra_DatePicker
