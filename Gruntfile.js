07e7de755657d035f6288e434916cdb147c881c3
Stefan Gabos <contact@stefangabos.ro>
2018-08-10 10:05:36 +0300

    Update required by the latest version of Uglify

--- Gruntfile.js
+++ Gruntfile.js
@@ -171,7 +171,10 @@ module.exports = function(grunt) {
             options: {
                 compress: true,
                 mangle: true,
-                beautify: false
+                beautify: false,
+                output: {
+                    ie8: true
+                }
             },
             build: {
                 src: 'src/zebra_datepicker.src.js',
